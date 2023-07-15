8881cada4059eb10c559ce64b4108acef2cd3164
idkwell <88640833+idkwell@users.noreply.github.com>
2023-07-15 20:51:38 +0900

    Add files via upload
    get_chatgpt_response는 chat gpt한테 요약정리를 시키는 함수이고
    split_text는 chat api 입력 토큰 길이 제한 때문에 분할해서 작업 수행하도록 하는 함수입니다.
    process_text를 통해 위 두 함수를 적용시킵니다.
    예시 : 
    input_text = "텍스트 입력 예시"
    processed_text = process_text(input_text)

---
+++ query.py
@@ -0,0 +1,56 @@
+openai.api_key = "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"
+
+def get_chatgpt_response(content):
+    prompt = f"""I will provide the text, so please summarize the contents in Korean in detail.
+\n\n```{content}```"""
+    
+    print(f"Sending prompt to GPT-3:\n{prompt}\n")
+    
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a deep learning model for learning assistance."},
+            {"role": "user", "content": prompt},
+        ],
+        max_tokens=300,
+        n=1,
+        temperature=0,
+    )
+
+    answer = response.choices[0].message['content'].strip()
+    return answer
+
+
+def split_text(text, limit=1700):
+    if len(text) <= limit:
+        return [text]
+
+    chunks = []
+    while text:
+        chunk = text[:limit]
+        last_space_index = chunk.rfind(' ')
+
+        # 추가: last_space_index가 -1이면 텍스트를 그대로 분할합니다.
+        if last_space_index == -1:
+            last_space_index = limit
+
+        chunk = chunk[:last_space_index]
+        chunks.append(chunk)
+        text = text[last_space_index:].strip()
+
+    return chunks
+
+
+def process_text(text):
+    # 텍스트 분할
+    content_chunks = split_text(text)
+
+    answers = []
+    for chunk in content_chunks:
+        answer = get_chatgpt_response(chunk)
+        answers.append(answer)
+        print(f"Received response for chunk: {answer}")  # 응답을 출력합니다.
+
+    # 결과를 하나의 문자열로 결합
+    combined_answer = ' '.join(answers)
+    return combined_answer
